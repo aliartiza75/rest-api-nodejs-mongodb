a6cbee713337f3f24987271bc13c3bdfea08af5b
irtiza <aliartiza75@yahoo.com>
2020-04-14 23:39:20 +0500

    [update-manifests] update Usermodel and add feedModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,10 +1,17 @@
 var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
+	userId: {type: String, required: true},
+	email: {type: String, required: true},
 	firstName: {type: String, required: true},
 	lastName: {type: String, required: true},
-	email: {type: String, required: true},
+	city: {type: String, required: true},
+	country: {type: String, required: true},
+	phone: {type: String, required: true},
 	password: {type: String, required: true},
+	lastLogin: {type: String, required: true},
+	lastName: {type: String, required: true},
+	lastSeenIp: {type: String, required: true},
 }, {timestamps: true});
 
 // Virtual for user's full name
